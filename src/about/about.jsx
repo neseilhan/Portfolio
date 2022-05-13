3c516642f362024139aa9b92498277e4e337dcb9
Neşe İLHAN <neseilhannn@gmail.com>
2022-05-13 16:53:43 +0300

    about

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
 import './about.css'
+import ME from '../needs/me2.png'
+import {BsAward} from 'react-icons/bs'
+import {MdCelebration} from 'react-icons/md'
+import {AiOutlineFolderOpen} from 'react-icons/ai'
+
 const about = () => {
   return (
-    <div>about</div>
+    <section id='about'>
+      <h2> About me</h2>
+      <div className='container about__container'>
+        <div className='about_me'>
+          <div className='about_me-image'>
+<img src={ME} alt="About Image"/>
+          </div>
+        </div>
+      <div className='about__content'>
+        <div className='abpıt__cards'>
+
+          <article className='about_card'>
+            <BsAward className='about__icon' size={25}/>
+            <h5>Experience</h5>
+            <small>3+ years working</small>
+          </article>
+
+          <article className='about_card'>
+            <MdCelebration className='about__icon' size={25}/>
+            <h5>Clients</h5>
+            <small>200+ Worldwide</small>
+          </article>
+
+          <article className='about_card'>
+            <AiOutlineFolderOpen className='about__icon' size={25}/>
+            <h5>Projects</h5>
+            <small>70+ Completed</small>
+          </article>
+
+        </div>
+
+      </div>
+
+      </div>
+    </section>
   )
 }
 
--- src/nav/nav.jsx
+++ src/nav/nav.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './nav.css'
-const nav = () => {
+import {BiHomeHeart} from "react-icons/bi"
+import {RiUser5Line} from "react-icons/ri"
+import {RiServiceLine} from 'react-icons/ri'
+import {BiBookBookmark} from "react-icons/bi"
+import {BiMessageAltDots} from "react-icons/bi"
+const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#")
   return (
-    <div>nav</div>
+    <nav>
+      <a href='#' ><BiHomeHeart size={25}/></a>
+      <a href='#about'  onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><RiUser5Line size={25}/></a>
+      <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''} ><BiBookBookmark size={25}/></a>
+      <a href='#services' onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''} ><RiServiceLine size={25}/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageAltDots size={25}/></a>
+    </nav>
   )
 }
 
-export default nav
+export default Nav
